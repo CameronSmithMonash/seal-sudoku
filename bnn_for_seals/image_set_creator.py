dcc81f7942fec83d8875564aded86e9c9360d53a
Ubuntu <ubuntu@lucille.novalocal>
2019-01-30 10:34:04 +0000

    updated files to work with cropped seal images

--- bnn_for_seals/image_set_creator.py
+++ bnn_for_seals/image_set_creator.py
@@ -2,14 +2,20 @@
 
 from shutil import copyfile
 import os
+import argparse
 
-directory = "seal_data/"
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--image_directory', type=str, default="label.db")
+opts = parser.parse_args()
+
+image_directory = opts.image_directory
+directory = "sample_data/"
 
 i = 0
-for image in os.listdir(directory + "all_images"):
+for image in os.listdir(directory + image_directory):
     if i % 5 == 0:
-        copyfile(directory + "all_images/" + image, directory + "test/" + image)
-        copyfile(directory + "all_images/" + image, directory + "unlabelled/" + image)
+        copyfile(directory + image_directory + image, directory + "test/" + image)
+        copyfile(directory + image_directory + image, directory + "unlabelled/" + image)
     else:
-        copyfile(directory + "all_images/" + image, directory + "training/" + image)
+        copyfile(directory + image_directory + image, directory + "training/" + image)
     i += 1
